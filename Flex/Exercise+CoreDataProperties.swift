ccc8bd0102374259f32717bf9c72b23f9e7e19e1
Varindra <varindrahart@gmail.com>
2016-03-20 19:15:58 -0400

    Adjusted exercise properties
    Now included repRange min/max

--- Flex/Exercise+CoreDataProperties.swift
+++ Flex/Exercise+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Exercise+CoreDataProperties.swift
 //  Flex
 //
-//  Created by Varindra Hart on 3/6/16.
+//  Created by Varindra Hart on 3/20/16.
 //  Copyright © 2016 Varindra Hart. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -16,6 +16,8 @@ extension Exercise {
 
     @NSManaged var name: String?
     @NSManaged var notes: String?
+    @NSManaged var repRangeMin: NSNumber?
+    @NSManaged var repRangeMax: NSNumber?
     @NSManaged var sets: NSOrderedSet?
     @NSManaged var workout: Workout?
 
---
+++ Flex/NSFetched.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Flex
+//
+//  Created by Varindra Hart on 3/17/16.
+//  Copyright © 2016 Varindra Hart. All rights reserved.
+//
+
+import Foundation
